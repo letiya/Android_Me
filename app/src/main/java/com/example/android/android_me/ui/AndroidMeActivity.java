8df4d0d2cd0383736ff4fd9e46866c3727e118a4
Belle Lee <letiya66@gmail.com>
2018-06-30 12:35:25 -0700

    3. Advanced Android App Development - Lesson 2 Fragments - 23. Exercise: Communicate Between Activities

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -16,6 +16,7 @@
 
 package com.example.android.android_me.ui;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -37,13 +38,15 @@ public class AndroidMeActivity extends AppCompatActivity {
 
             // TODO (5) Retrieve list index values that were sent through an intent; use them to display the desired Android-Me body part image
                 // Use setListindex(int index) to set the list index for all BodyPartFragments
+            Intent intentStartedThisActivity = getIntent();
 
             // Create a new head BodyPartFragment
             BodyPartFragment headFragment = new BodyPartFragment();
 
             // Set the list of image id's for the head fragment and set the position to the second image in the list
             headFragment.setImageIds(AndroidImageAssets.getHeads());
-            headFragment.setListIndex(1);
+            int headIndex = intentStartedThisActivity.getIntExtra("headIndex", 0);
+            headFragment.setListIndex(headIndex);
 
             // Add the fragment to its container using a FragmentManager and a Transaction
             FragmentManager fragmentManager = getSupportFragmentManager();
@@ -56,11 +59,17 @@ public class AndroidMeActivity extends AppCompatActivity {
 
             BodyPartFragment bodyFragment = new BodyPartFragment();
             bodyFragment.setImageIds(AndroidImageAssets.getBodies());
+            int bodyIndex = intentStartedThisActivity.getIntExtra("bodyIndex", 0);
+            headFragment.setListIndex(bodyIndex);
+
             fragmentManager.beginTransaction()
                     .add(R.id.body_container, bodyFragment)
                     .commit();
 
             BodyPartFragment legFragment = new BodyPartFragment();
+            int legIndex = intentStartedThisActivity.getIntExtra("legIndex", 0);
+            headFragment.setListIndex(legIndex);
+
             legFragment.setImageIds(AndroidImageAssets.getLegs());
             fragmentManager.beginTransaction()
                     .add(R.id.leg_container, legFragment)
